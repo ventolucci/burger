5300e506bc9c4cd30d063ac004820b9d96c0c681
Denis Rodriguez-Ramos <ventolucci@hotmail.com>
2018-04-25 12:40:51 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  port: 8888,
-  database: "burger_db"
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: 8888,
+    database: "burger_db"
+  });
+};
+
 
 // Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
